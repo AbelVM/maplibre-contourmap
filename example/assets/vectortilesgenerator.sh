4e7f288fcf8d46fb19d0aa0482e7ec0173cb0216
Abel Vázquez Montoro <abel.vm@gmail.com>
2024-10-12 13:24:26 +0200

    backup
    Signed-off-by: Abel Vázquez Montoro <abel.vm@gmail.com>

--- example/assets/vectortilesgenerator.sh
+++ example/assets/vectortilesgenerator.sh
@@ -1,2 +1,10 @@
+# folders tree
 ogr2ogr -f MVT ./tiles ./cotas.gpkg -dsco MINZOOM=0 -dsco MAXZOOM=15 -dsco COMPRESS=NO -dsco NAME=cotas -dsco TYPE=overlay
+
+# pmtiles ogr2ogr
+ogr2ogr -dsco MINZOOM=5 -dsco MAXZOOM=15 -f "PMTiles" cotas.pmtiles cotas_cleaned.gpkg
+
+# pmtiles tippecanoe, avg clustered at 10px radius
+ogr2ogr -t_srs EPSG:4326 cotas.json cotas.gpkg
+tippecanoe -zg --projection=EPSG:4326 -o cotas.pmtiles -r1 --cluster-distance=10 --accumulate-attribute=COTA:mean -l cotas cotas.json
 
--- example/example.js
+++ example/example.js
@@ -5,64 +5,70 @@ import init from '../dist/maplibre-contourmap.js';
 // add the add/remove functions to Map prototype
 init(maplibregl);
 
-const map = new maplibregl.Map({
-    container: 'map',
-    style: 'https://demotiles.maplibre.org/style.json', 
-    center: [-2.9945918,39.8983039,0],
-    zoom: 10
-});
-map.on('load', () => {
+const protocol = new pmtiles.Protocol();
+maplibregl.addProtocol('pmtiles', protocol.tile);
+const PMTILES_URL = location.href.replace('index.html', 'assets/cotas.pmtiles');
+const p = new pmtiles.PMTiles(PMTILES_URL);
+protocol.add(p);
+p.getHeader().then(h => {
+    const map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://demotiles.maplibre.org/style.json',
+        zoom: 5,//h.maxZoom - 2,
+        center: [-3, 41] //[h.centerLon, h.centerLat]
+    });
+    map.on('load', () => {
+        // Raw points layer
+        map.addSource('points_source', {
+            type: 'vector',
+            url: `pmtiles://${PMTILES_URL}`
+        });
+        map.addLayer({
+            'id': 'points_layer',
+            'type': 'circle',
+            'source': 'points_source',
+            'source-layer': 'cotas',
+            'paint': {
+                'circle-radius': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    5, 2.5,
+                    19, 30
+                ],
+                'circle-color': [
+                    'interpolate-lab',
+                    ['linear'],
+                    ['get', 'COTA'],
+                    116, '#36a0a4',
+                    342, '#73829b',
+                    561, '#c05e90',
+                    765, '#ff5180',
+                    981, '#ff876c',
+                    1314, '#ffb65a',
+                    3360, '#ffdb4c'
+                ]
+            }
+        });
 
-    // Raw points layer
-    map.addSource('points_source', {
-        type: 'vector',
-        tiles:[location.href.replace('index.html','assets/tiles/{z}/{x}/{y}.pbf')]
-    });
-    map.addLayer({
-        'id': 'points_layer',
-        'type': 'circle',
-        'source': 'points_source',
-        'source-layer': 'cotas',
-        'paint': {
-            'circle-radius': [
-                'interpolate',
-                ['linear'],
-                ['zoom'],
-                5, 2.5,
-                19, 30
-            ],
-            'circle-color': [
-                  'interpolate-lab',
-                  ['linear'],
-                  ['get', 'COTA'],
-                  116,'#36a0a4',
-                  342,'#73829b',
-                  561,'#c05e90',
-                  765,'#ff5180',
-                  981,'#ff876c',
-                  1314,'#ffb65a',
-                  3360,'#ffdb4c'
-              ]
-        }
-    });
+        map.addContourSource('points_layer', {
+            'measure': 'COTA',
+            'breaks': [116, 342, 561, 765, 981, 1314, 3360]
+        });
 
-    map.addContourSource('points_layer',{
-        'measure': 'COTA',
-        'breaks': [116,342,561,765,981,1314,3360]
-    });
+        map.addLayer({
+            'id': 'cells',
+            'type': 'fill',
+            'source': 'contour-source-points_layer',
+            'layout': {},
+            'paint': {
+                'fill-color': '#ccc',
+                'fill-outline-color': '#000',
+                'fill-opacity': 0.25
+            }
+        },'points_layer');
 
-    map.addLayer({
-        'id': 'maine',
-        'type': 'fill',
-        'source': 'contour-source-points_layer',
-        'layout': {},
-        'paint': {
-            'fill-color': '#088',
-            'fill-outline-color': '#000',
-            'fill-opacity': 0.8
-        }
+        //debugger
+        window.map = map;
     });
-    
-    //debugger
-    window.map=map;
 });
